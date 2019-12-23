1866d756fb71bb2a78289c56f4f40025fa646cbf
kamleong <kamleong@users.noreply.github.com>
2019-12-23 13:59:07 +0800

    Update MainActivity.java

--- src/main/java/com/quoinsight/minimal/MainActivity.java
+++ src/main/java/com/quoinsight/minimal/MainActivity.java
@@ -112,11 +112,12 @@ public class MainActivity extends Activity {
 
     TextView txt9 = new TextView(this);
       txt9.setGravity(Gravity.CENTER_HORIZONTAL);
+      txt9.setText(android.text.Html.fromHtml(
+        " [ <A href=\"https://github.com/QuoInsight/minimal.apk\">src</A> ]"
+      ));
+      // below must set only after setText() !
       txt9.setAutoLinkMask(android.text.util.Linkify.ALL);  txt9.setLinksClickable(true);
       txt9.setMovementMethod(android.text.method.LinkMovementMethod.getInstance());
-      txt9.setText(android.text.Html.fromHtml(
-        "[ <A href=\"https://github.com/QuoInsight/minimal.apk\">src</A> ]"
-      ));
 
     LinearLayout layout = new LinearLayout(this);
       layout.setGravity(Gravity.CENTER);  // Gravity.CENTER
